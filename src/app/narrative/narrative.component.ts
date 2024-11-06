4c24bf99e4261ec91b0f895060154f006e94f0b1
Dang Nguyen <dang.nguyen@pureintegration.com>
2024-11-06 13:08:20 -0500

    sticky + spinner work

--- src/app/narrative/narrative.component.ts
+++ src/app/narrative/narrative.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { GlobalSpinnerComponent } from 'src/example/components/global-spinner.component';
 
 @Component({
   selector: 'app-narrative',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatProgressSpinnerModule, GlobalSpinnerComponent],
   templateUrl: './narrative.component.html',
   styleUrl: './narrative.component.scss'
 })
 export class NarrativeComponent {
+  isLoading = false;
+  selectedPage: string;
+  pageContent = {
+    1: 'Content for Page 1',
+    2: 'Content for Page 2',
+    3: 'Content for Page 3'
+  };
+  isSticky = false;
 
+  @HostListener('window:scroll', [])
+  checkSticky() {
+    const stickyElement = document.querySelector('.sticky-action-buttons');
+    if (stickyElement) {
+      const stickyOffset = (stickyElement as HTMLElement).offsetTop;
+      this.isSticky = window.pageYOffset >= stickyOffset;
+    }
+  }
+  onPageChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.selectedPage = target.value || '1';
+  }
+
+
+  save() {
+    this.isLoading = true;
+    // Simulate backend call with a timeout
+    setTimeout(() => {
+      this.isLoading = false;
+      // Handle successful save or display errors as needed
+      console.log('Saved!');
+    }, 2000); // Simulate 2-second delay
+  }
 }
